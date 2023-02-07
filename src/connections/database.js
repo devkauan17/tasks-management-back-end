d1970897c6488912803a3b0eb7a5a668aedf2bbe
Kauan Rodrigues <kauanrdx145@gmail.com>
2023-02-06 21:43:06 -0300

    adiconado o DB_URL

--- src/connections/database.js
+++ src/connections/database.js
@@ -2,14 +2,7 @@ require("dotenv").config();
 
 const knex = require("knex")({
     client: "pg",
-    connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DB_URL
 });
 
 module.exports = knex;
